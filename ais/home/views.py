ec39603a55a59e0ece91a8f2be70098417dec0a5
M-JV <melvinjohn2001@gmail.com>
2023-06-08 19:18:16 +0530

    navset

--- ais/home/views.py
+++ ais/home/views.py
@@ -5,4 +5,11 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'home/index.html')
 
+def about(request):
+    # Your about view code here
+    return render(request, 'home/about.html')
+
+def contact(request):
+    # Your contact view code here
+    return render(request, 'home/contact.html')
 
